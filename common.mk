7458b26577d07d3b1a6a8cd01aa707bfdc036806
Arian <arian.kulmer@web.de>
2025-02-18 18:00:35 +0100

    sm8650-common: Generate qcrilNr.db during build
    Change-Id: I1f3425713b9b5b1cc7185bc94124b9f01efe3abb

--- common.mk
+++ common.mk
@@ -333,6 +333,9 @@ PRODUCT_PACKAGES += \
     telephony-ext \
     xiaomi-telephony-stub
 
+PRODUCT_PACKAGES += \
+    qcrilNrDb_vendor
+
 PRODUCT_BOOT_JARS += \
     telephony-ext \
     xiaomi-telephony-stub
