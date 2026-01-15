b4388472066b3e24f3d85f6255dd82624f9b6308
klozz <carlosj@klozz.dev>
2026-01-15 01:43:56 +0100

    [REVERTME]zorn: permissive
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,7 +106,7 @@ BOARD_BOOTCONFIG := \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     androidboot.vendor.qspa=true \
-    androidboot.console=0
+    androidboot.console=0 androidboot.selinux=permissive
 
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
